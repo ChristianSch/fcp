3da2aab45bee916c5aea946506010ab3775ed939
Christian Schulze <christian.schulze@mni.thm.de>
2014-10-15 11:06:08 +0200

    Lösung der zweiten Aufgabe

---
+++ Blatt2/Aufgabe2.clj
@@ -0,0 +1,45 @@
+; Ausdrücke in der Präfix-Notation
+; Schreiben Sie folgende Ausdrücke in der Präfix-Notation:
+
+; (a) 2 + 3 + 1
+;
+; + + 2 3 1
+;
+; in Clojure:
+(+ (+ 2 3) 1)
+
+; (b) 2 * 3 + 4
+;
+; + * 2 3 4
+;
+; in Clojure:
+(+ (* 2 3) 4)
+
+; (c) 2 * (3 + 4)
+;
+; * 2 + 3 4
+;
+; in Clojure:
+(* 2 (+ 3 4))
+
+; (d) true and false or true
+;
+; and true or false true
+;
+; in Clojure:
+(and true (or false true))
+
+; (e) square(7) - 7
+;
+; - square(7) 7
+
+; (f) (1 = 1) or (1 != 1)
+;
+; or = 1 1 != 1 1
+
+; (g) (3 + 4) * (2 + 5)
+;
+; * + 3 4 + 2 5
+;
+; in Clojure:
+(* (+ 3 4) (+ 2 5))
