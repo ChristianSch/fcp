aadad279a49a6749110a1604b74ccabdff53ce86
Christian Schulze <christian.schulze@mni.thm.de>
2014-10-15 13:58:58 +0200

    Anfang Loesung zu Aufgabe 4

---
+++ Blatt2/Aufgabe4.clj
@@ -0,0 +1,5 @@
+; (a) Wie kann man die Liste (+ 2 3) als eine Liste von Werten behandeln?
+
+(seq (+ 2 3))
+
+; (b) Und wie kann man eine Liste (als Daten) mit den Elementen +, 2 und 3 als Code ausfuehren?
