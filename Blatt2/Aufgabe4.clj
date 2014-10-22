9896b6d983e7c56d3a80a2f817d815b335f6c69e
Christian Schulze <christian.schulze@mni.thm.de>
2014-10-22 08:41:36 +0200

    solved excercise 4

--- Blatt2/Aufgabe4.clj
+++ Blatt2/Aufgabe4.clj
@@ -3,3 +3,11 @@
 (seq (+ 2 3))
 
 ; (b) Und wie kann man eine Liste (als Daten) mit den Elementen +, 2 und 3 als Code ausfuehren?
+;
+; * Liste
+;
+;   (list + 2 3)
+;
+; * Ausfuehrung der Liste
+;
+;   (eval (list + 3 4))
