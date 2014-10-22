2752aea7a3235d7896666e52be41e34e1567cdc1
Christian Schulze <christian.schulze@mni.thm.de>
2014-10-22 09:52:16 +0200

    improved solution to excercise 4

--- Blatt2/Aufgabe4.clj
+++ Blatt2/Aufgabe4.clj
@@ -1,14 +1,15 @@
 ; (a) Wie kann man die Liste (+ 2 3) als eine Liste von Werten behandeln?
-;
-; (quote (+ 2 3))
+
+'(+ 2 3)
 
 ; (b) Und wie kann man eine Liste (als Daten) mit den Elementen +, 2 und 3 als Code ausfuehren?
-;
-; '(+ 2 3) oder (quote (+ 2 3))
-;
-; (def l '(+ 2 3))
-;
+
+'(+ 2 3)
+
+; oder
+(quote (+ 2 3))
+
+(def l '(+ 2 3))
+
 ; und ausfuehren:
-;
-; (eval l)
-;
+(eval l)
