c7a6c1e96a8f7fb8e9cc027ebb3f1c4b5283e9c3
Christian Schulze <christian.schulze@mni.thm.de>
2014-10-29 10:17:55 +0100

    reformatted

--- Blatt3/Aufgabe2.clj
+++ Blatt3/Aufgabe2.clj
@@ -4,16 +4,22 @@
 ; REPL einige Beispiele für die Verwendung der definierten Funktionen:
 
 ;  square mit x􏰀 → x × x
-(defn square "Quadriert die uebergebene Zahl" [x] (* x x))
+(defn square [x]
+  "Quadriert die uebergebene Zahl"
+  (* x x))
 (square 2) ; => 4
 (square -1) ; => 1
 
 ; sum-of-squares mit (x,y)􏰀 → x × x + y × y
-(defn sum-of-squares "Summiert die Quadrate der uebergebenen Werte" [x,y] (+ (* x x) (* y y)))
+(defn sum-of-squares [x,y]
+  "Summiert die Quadrate der uebergebenen Werte"
+  (+ (* x x) (* y y)))
 (sum-of-squares 2 3)
 (sum-of-squares 1 1)
 
 ; eval-test mit (x, y) 􏰀→ x × x
-(defn eval-test [x,y] (* x x))
+(defn eval-test
+  [x,y]
+  (* x x))
 (eval-test 2 3)
 (eval-test 1 100)
