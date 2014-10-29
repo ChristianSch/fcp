a8e1b01de0966267a2e7351b0a47437a3a7ab387
Christian Schulze <christian.schulze@mni.thm.de>
2014-10-29 11:01:32 +0100

    removed wrong solutions; TODO

--- Blatt3/Aufgabe3.clj
+++ Blatt3/Aufgabe3.clj
@@ -1,24 +1,29 @@
 (ns aufgabe3)
 
+
+; TODO
+
 ; Funktion aus der vorherigen Aufgabe
 (defn eval-test [x,y] (* x x))
 
-; 1. 2 => 2
-; 2. 2 * 2 => 4
+; 1. (* 2 2)
+; 2. 4
+;
+; verzögert: 2 schritte
+; strikt: 2 schritte
 (eval-test 2 3)
 
-; 1. 3 => 3
-; 2. 4 => 4
-; 3. 3 + 4 => 7
-; 4. 7 * 7 => 49
+; 1. (* (+ 3 4) (+ 3 4))
+; 2. (* 7 (+ 3 4))
+; 3. (* 7 7)
+; 4. 49
+
+; verzögert: 4
+; strikt: 3
 (eval-test (+ 3 4) 8)
 
-; 1. 7 => 7
-; 2. 7 * 7 => 49
+; verzögert: 3
+; strikt: 4
 (eval-test 7 (* 2 4))
 
-; 1. 3 => 3
-; 2. 4 => 4
-; 3. 3 + 4 => 7
-; 4. 7 * 7 => 49
 (eval-test (+ 3 4) (* 2 4))
